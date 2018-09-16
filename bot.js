a58ea5f88006d4e3f50ad8f2bce54b329c097f0d
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-16 20:14:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1498,8 +1498,6 @@ client.on('message' , message => {
     }
 });
 
-const Discord = require('discord.js');
-	const client = new Discord.Client()
 	const fs = require('fs');
 	const moment = require('moment');
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
